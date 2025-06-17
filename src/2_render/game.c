665342d1e4026367032d411b71be575fbe75306b
Liyuan <58136495+Liyuan701@users.noreply.github.com>
2025-06-17 23:14:21 +0200

    Update game.c
    Mar

--- src/2_render/game.c
+++ src/2_render/game.c
@@ -12,7 +12,7 @@
 
 #include "../include/cub3D.h"
 
-void	ft_refresh(t_game *game) // draw_loop
+void	ft_refresh(t_game *game)
 {
 	double	fraction;
 	double	dir;
@@ -22,13 +22,17 @@ void	ft_refresh(t_game *game) // draw_loop
 	//TODO: draw_2D;
 	i = 0;
 	fraction = PI / 3 / WIDTH;
-	dir = game->player->angle - PI / 6; 
-	ft_draw_ray(game, player, angle, column);
-	dir += fraction;
-	i++;
-	return (0);
+	dir = game->player.angle - PI / 6;
+	while (i < WIDTH)
+        {
+	    ft_draw_ray(game, game->player, dir, i);
+	    dir += fraction;
+	    i++;
+	}
+	    mlx_put_image_to_window(game->mlx, game->win, game->img, 0, 0);
+	return ;
+	}
 }
-
 
 void	ft_open_window(t_game *game)
 {
